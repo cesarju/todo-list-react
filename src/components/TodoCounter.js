130d08c5fcb112c021ec9394a37a3538845f3f45
Cesar Ticona <cesarju.cc@gmail.com>
2022-04-22 00:46:05 -0400

    completed to search todo list

--- src/components/TodoCounter.js
+++ src/components/TodoCounter.js
@@ -5,7 +5,7 @@ function TodoCounter({ todo, completed }) {
   return (
     <div className="TodoCounter">
       <h2>Simplest</h2>
-      <h3> TASK LIST</h3>
+      <h3>TASK LIST</h3>
       <h4>
         Haz completado {completed} de {todo} tareas
       </h4>
